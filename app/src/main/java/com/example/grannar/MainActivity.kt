cf06361a088c2b8e0e2a8b5c32d5a8832d601c81
Jonas Bondesson <jonasbondesson@yahoo.com>
2024-02-09 10:23:30 +0100

    added navBar

--- app/src/main/java/com/example/grannar/MainActivity.kt
+++ app/src/main/java/com/example/grannar/MainActivity.kt
@@ -2,15 +2,20 @@ package com.example.grannar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Kristian test
 
-        //Frida Test
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = findNavController(R.id.nav_host_fragment)
 
-        //Test2 Frida
+        bottomNav.setupWithNavController(navController)
+
+
     }
 }
