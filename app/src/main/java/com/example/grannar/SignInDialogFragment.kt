7557f985026e922a71c1407ac1938d924d081ea1
KT <thun.kristian@gmail.com>
2024-02-09 11:59:15 +0100

    Logging in works

--- app/src/main/java/com/example/grannar/SignInDialogFragment.kt
+++ app/src/main/java/com/example/grannar/SignInDialogFragment.kt
@@ -3,17 +3,33 @@ package com.example.grannar
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInDialogFragment() : DialogFragment() {
+
+    lateinit var emailEditText: EditText
+    lateinit var passwordEditText: EditText
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val inflater = LayoutInflater.from(requireContext())
         val view = inflater.inflate(R.layout.sign_in_dialog_fragment, null)
-        val emailEditText = view.findViewById<EditText>(R.id.signInEmailEditText)
-        val passwordEditText = view.findViewById<EditText>(R.id.signInPasswordEditText)
+        emailEditText = view.findViewById(R.id.signInEmailEditText)
+        passwordEditText = view.findViewById(R.id.signInPasswordEditText)
+        val singInButton = view.findViewById<Button>(R.id.signInButton)
+        val cancelButton = view.findViewById<Button>(R.id.cancelSignInButton)
+
+        cancelButton.setOnClickListener {
+            dismiss()
+        }
+
+        singInButton.setOnClickListener {
+            signIn()
+        }
 
 
 
@@ -25,5 +41,26 @@ class SignInDialogFragment() : DialogFragment() {
         return builder.create()
 
     }
+
+    private fun signIn() {
+        val email = emailEditText.text.toString()
+        val password = passwordEditText.text.toString()
+
+
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                Log.d("!!!", "Logged in")
+                dismiss()
+        }
+            .addOnFailureListener {
+                Log.d("!!!", "Failure logging in!")
+            }
+
+
+    }
+
+
+
 
 }
