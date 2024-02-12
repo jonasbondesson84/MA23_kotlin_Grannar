066f330cffc501300e47e99c03da7ff50f8de9d2
Fridisen <fil.gustavsson@gmail.com>
2024-02-12 15:12:36 +0100

    fgProfil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,6 @@ dependencies {
 
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0") //för att kunna skapa en cirkel
 }
