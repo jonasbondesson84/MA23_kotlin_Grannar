c20651b9c2e7ef611f0bffb21baacd96a91c3db5
Jonas Bondesson <jonasbondesson@yahoo.com>
2024-02-12 11:01:43 +0100

    added currentUser singleton

--- app/src/main/java/com/example/grannar/CurrentUser.kt
+++ app/src/main/java/com/example/grannar/CurrentUser.kt
@@ -1,6 +1,21 @@
 package com.example.grannar
 
+import com.google.type.LatLng
+import java.time.LocalDate
+
 object CurrentUser {
 
+    var userID: String? = null
+    var firstName: String? = null
+    var surname: String? = null
+    var age: LocalDate? = null
+    var location: LatLng? = null
+    var email: String? = null
+    var gender: String? = null
+    var profileImageURL: String? = null
+    var interests = mutableListOf<Interests>()
+    var aboutMe: String? = null
+    var imageURLs = mutableListOf<String>()
+    var friendsList = mutableListOf<User>()
 
 }
