a80d5f70536a5d385afdca759948413c3fd3e77b
WedSal <wed.salman18@gmail.com>
2024-02-20 11:07:41 +0100

    fixed code

--- app/src/main/java/com/example/grannar/ProfileFragment.kt
+++ app/src/main/java/com/example/grannar/ProfileFragment.kt
@@ -9,11 +9,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.firestore
@@ -50,6 +53,9 @@ class ProfileFragment : Fragment(), AddedInterestCallback{
     private lateinit var lastInterestImageView: ImageView
     val MAX_INTERESTS = 6
 
+    private lateinit var selectedImageUri: Uri
+    private lateinit var getContent: ActivityResultLauncher<String>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -58,6 +64,13 @@ class ProfileFragment : Fragment(), AddedInterestCallback{
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
             }
+        getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri:
+                                                                                       Uri? -> uri?.let {
+            selectedImageUri = it
+        }
+        }
+
+
         }
 
 
@@ -109,6 +122,11 @@ class ProfileFragment : Fragment(), AddedInterestCallback{
         lastInterestImageView = view.findViewById(R.id.deleteInterest6ImageView)
 
         Log.d("!!!", "${CurrentUser.interests?.size}")
+
+        val button: ImageButton = view.findViewById(R.id.chooseImageButton)
+        button.setOnClickListener{
+            openGallery()
+        }
 
 
 
@@ -319,6 +337,11 @@ class ProfileFragment : Fragment(), AddedInterestCallback{
     }
     override fun interestAdded() {
         showInterestsWithColor(CurrentUser.interests)
+    }
+
+
+    fun openGallery() {
+        getContent.launch("image/*")
     }
 
 private fun saveAboutMe(newAboutMe: String) {
