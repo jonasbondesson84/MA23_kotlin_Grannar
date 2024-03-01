3e58618a4ef5f85e8bce77151f8c28edd15ea76d
Jonas Bondesson <jonasbondesson@yahoo.com>
2024-03-01 13:18:12 +0100

    fixed sinup error check

--- app/src/main/java/com/example/grannar/SignUpActivity.kt
+++ app/src/main/java/com/example/grannar/SignUpActivity.kt
@@ -5,10 +5,15 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
+import android.util.Patterns
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
@@ -21,7 +26,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.datepicker.CalendarConstraints
 import com.google.android.material.datepicker.DateValidatorPointBackward
 import com.google.android.material.datepicker.MaterialDatePicker
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.text.SimpleDateFormat
@@ -39,12 +44,124 @@ class SignUpActivity : AppCompatActivity(), OnDataPassListener {
     lateinit var emailEditText: EditText
     lateinit var genderRadioGroup: RadioGroup
     lateinit var birthdayEditText: EditText
+    private lateinit var emailTextLayout: TextInputLayout
+    private lateinit var confPWTextLayout: TextInputLayout
+    private lateinit var passwordTextLayout: TextInputLayout
+    private lateinit var imFirstNameCheck: ImageView
+    private lateinit var imSurNameCheck: ImageView
+    private lateinit var imLocationCheck: ImageView
+    private lateinit var imBirthdayCheck: ImageView
+    private lateinit var imGenderCheck: ImageView
+    private lateinit var imEmailCheck: ImageView
+    private lateinit var imPasswordCheck: ImageView
+    private lateinit var imConfirmPWCheck: ImageView
+    private lateinit var btnSignUp: Button
+
     var birthDate: Date? = null
     //var mapFragment: MapFragment? = null
     private var location: LatLng? = null
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var userLocation: LatLng? = null
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+
+            val firstName = firstNameEditText.text.toString()
+            val surName = surNameEditText.text.toString()
+
+            if(firstName.length >2) {
+                imFirstNameCheck.visibility = View.VISIBLE
+            } else {
+                imFirstNameCheck.visibility = View.INVISIBLE
+            }
+            if(surName.length >2) {
+                imSurNameCheck.visibility = View.VISIBLE
+            } else {
+                imSurNameCheck.visibility = View.INVISIBLE
+            }
+
+
+            btnSignUp.isEnabled = checkIfAllDataIsCorrect()
+
+        }
+
+        override fun afterTextChanged(s: Editable?) {}
+
+    }
+
+    private fun checkIfAllDataIsCorrect(): Boolean {
+
+        return (firstNameEditText.text.length > 2 &&
+                surNameEditText.text.length > 2 &&
+                location != null &&
+                birthDate != null &&
+                genderRadioGroup.checkedRadioButtonId != -1 &&
+                emailTextLayout.error == null &&
+                emailEditText.text.isNotEmpty() &&
+                passwordTextLayout.error == null &&
+                passwordEditText.text.isNotEmpty() &&
+                confPWTextLayout.error == null &&
+                confirmPasswordEditText.text.isNotEmpty()
+
+                )
+    }
+
+    private val textWatcherEmail = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val email = emailEditText.text.toString()
+            if(Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                emailTextLayout.error = null
+                imEmailCheck.visibility = View.VISIBLE
+            } else {
+                emailTextLayout.error = "No email-address"
+                imEmailCheck.visibility = View.INVISIBLE
+            }
+
+            btnSignUp.isEnabled = checkIfAllDataIsCorrect()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
+    }
+    private val textWatcherPassword = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val password = passwordEditText.text.toString()
+            val confirmationPassword = confirmPasswordEditText.text.toString()
+            if(password.length > 7) {
+                passwordTextLayout.error = null
+                imPasswordCheck.visibility = View.VISIBLE
+            } else {
+                passwordTextLayout.error = "Password must be at least 8 characters long"
+                imPasswordCheck.visibility = View.INVISIBLE
+            }
+            if(password == confirmationPassword && confirmationPassword.length > 7) {
+                confPWTextLayout.error = null
+                imConfirmPWCheck.visibility = View.VISIBLE
+            } else {
+                confPWTextLayout.error = "Password doesnt match"
+                imConfirmPWCheck.visibility = View.INVISIBLE
+            }
+
+            btnSignUp.isEnabled = checkIfAllDataIsCorrect()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
+    }
 
 
 
@@ -61,11 +178,39 @@ class SignUpActivity : AppCompatActivity(), OnDataPassListener {
         confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText)
         emailEditText = findViewById(R.id.emailEditText)
         genderRadioGroup = findViewById(R.id.genderRadioGroup)
+        birthdayEditText = findViewById(R.id.birthDateEditText)
 
+        emailTextLayout = findViewById(R.id.emailTextInputLayout)
+        confPWTextLayout = findViewById(R.id.confirmPasswordTextInputLayout)
+        passwordTextLayout = findViewById(R.id.passwordTextInputLayout)
+
+        imFirstNameCheck = findViewById(R.id.imFirstNameCheck)
+        imSurNameCheck = findViewById(R.id.imSurNameCheck)
+        imLocationCheck = findViewById(R.id.imLocationCheck)
+        imBirthdayCheck = findViewById(R.id.imBirthdayCheck)
+        imGenderCheck = findViewById(R.id.imGenderCheck)
+        imEmailCheck = findViewById(R.id.imEmailCheck)
+        imPasswordCheck = findViewById(R.id.imPasswordCheck)
+        imConfirmPWCheck = findViewById(R.id.imConfirmPasswordCheck)
+
+        firstNameEditText.addTextChangedListener(textWatcher)
+        surNameEditText.addTextChangedListener(textWatcher)
+        passwordEditText.addTextChangedListener(textWatcherPassword)
+        confirmPasswordEditText.addTextChangedListener(textWatcherPassword)
+        emailEditText.addTextChangedListener(textWatcherEmail)
+        birthdayEditText.addTextChangedListener(textWatcher)
+
+        btnSignUp = findViewById(R.id.signUpButton)
         //mapFragment = supportFragmentManager.findFragmentById(R.id.mapView) as MapFragment?
 
 
-        birthdayEditText = findViewById(R.id.birthDateEditText)
+
+        genderRadioGroup.setOnCheckedChangeListener { group, checkedId ->
+            Log.d("!!!", "what")
+            imGenderCheck.visibility = View.VISIBLE
+            btnSignUp.isEnabled = checkIfAllDataIsCorrect()
+        }
+
         birthdayEditText.apply {
             setOnClickListener {
                 showDatePickerDialog()
@@ -91,26 +236,27 @@ class SignUpActivity : AppCompatActivity(), OnDataPassListener {
         }
 
         findViewById<Button>(R.id.signUpButton).setOnClickListener {
-            val password = passwordEditText.text.toString()
-            val confirmationPassword = confirmPasswordEditText.text.toString()
-
-            if (checkInformation()) {
-                if (checkPassword(password, confirmationPassword)) {
-                    signUp()
-                } else {
-                    Snackbar.make(
-                        confirmPasswordEditText,
-                        "Passwords don't match",
-                        Snackbar.LENGTH_SHORT
-                    ).show()
-                }
-            } else {
-                Snackbar.make(
-                    confirmPasswordEditText,
-                    "Please fill out all fields",
-                    Snackbar.LENGTH_SHORT
-                ).show()
-            }
+            signUp()
+//            val password = passwordEditText.text.toString()
+//            val confirmationPassword = confirmPasswordEditText.text.toString()
+//
+//            if (checkInformation()) {
+//                if (checkPassword(password, confirmationPassword)) {
+//                    signUp()
+//                } else {
+//                    Snackbar.make(
+//                        confirmPasswordEditText,
+//                        "Passwords don't match",
+//                        Snackbar.LENGTH_SHORT
+//                    ).show()
+//                }
+//            } else {
+//                Snackbar.make(
+//                    confirmPasswordEditText,
+//                    "Please fill out all fields",
+//                    Snackbar.LENGTH_SHORT
+//                ).show()
+//            }
         }
     }
 
@@ -256,6 +402,8 @@ Log.d("!!!", "the")
                     SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(selectedDate)
                 birthdayEditText.setText(formattedDate)
                 birthDate = selectedDate
+                imBirthdayCheck.visibility = View.VISIBLE
+                btnSignUp.isEnabled = checkIfAllDataIsCorrect()
             }
         }
 
@@ -290,6 +438,8 @@ Log.d("!!!", "the")
 
     override fun onDataPassed(data: LatLng) {
         location = data
+        imLocationCheck.visibility = View.VISIBLE
+        btnSignUp.isEnabled = checkIfAllDataIsCorrect()
         Log.d("!!!","Data från dialogfragment ${location}")
     }
 
