61133394f193102d659dfeafe554e91a90f68ec7
KT <thun.kristian@gmail.com>
2024-03-04 09:43:17 +0100

    Added so the friend profile image shows and that you cant type in about of friend

--- app/src/main/java/com/example/grannar/FriendProfileFragment.kt
+++ app/src/main/java/com/example/grannar/FriendProfileFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
@@ -13,6 +14,7 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.chip.Chip
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -47,8 +49,8 @@ class FriendProfileFragment : Fragment() {
     private lateinit var tvAboutMe: TextView
     private var interestChips = mutableListOf<Chip>()
     private var selectedUser: User? = null
-    private var interestsTextViewList = mutableListOf<TextView>()
     private lateinit var appBar: MaterialToolbar
+    private lateinit var ivFriendProfile: ImageView
 
 
 
@@ -81,6 +83,8 @@ class FriendProfileFragment : Fragment() {
         interestChips.add(view.findViewById(R.id.friendsInterest4Chip))
         interestChips.add(view.findViewById(R.id.friendsInterest5Chip))
         interestChips.add(view.findViewById(R.id.friendsInterest6Chip))
+
+        ivFriendProfile = view.findViewById(R.id.friendProfileImageView)
 
 
 
@@ -215,6 +219,14 @@ class FriendProfileFragment : Fragment() {
         showInterests(selectedUser.interests)
 
         setIcons(selectedUser)
+        showProfileImage(selectedUser)
+
+    }
+
+    private fun showProfileImage(selectedUser: User){
+        Glide.with(this)
+            .load(selectedUser.profileImageURL)
+            .into(ivFriendProfile)
 
     }
 
